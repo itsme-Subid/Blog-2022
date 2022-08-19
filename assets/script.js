d29543be47bf4dbfb5672f2f9d8507f573bb9f0d
SUBID DAS <itsmesubid@gmail.com>
2022-08-19 20:53:07 +0530

    Added Projects and Contact

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@
     let logo = document.querySelector('.logo');
     let logoLg = document.querySelector('.logo-lg');
 
-    let body = document.querySelector('body'); 
+    let body = document.querySelector('body');
     body.onresize = () => {
         mediaQuery();
     };
@@ -72,4 +72,51 @@
             tr.classList.add('odd-tr');
         }
     });
+
+    // contact
+    window.addEventListener("DOMContentLoaded", function () {
+        // get the form elements defined in your form HTML above
+
+        let form = document.getElementById("my-form");
+        let status = document.getElementById("status");
+
+        // Success and Error functions for after the form is submitted
+        function success() {
+            form.reset();
+            status.innerHTML = "Thanks! Your form has been successfully submitted.";
+            setTimeout(hideMsg, 3000);
+        }
+
+        function error() {
+            status.innerHTML = "Oops! There was a problem.";
+            setTimeout(hideMsg, 3000);
+        }
+
+        function hideMsg() {
+            status.innerHTML = "";
+        }
+
+        // handle the form submission event
+        form.addEventListener("submit", function (ev) {
+            ev.preventDefault();
+            let data = new FormData(form);
+            ajax(form.method, form.action, data, success, error);
+        });
+    });
+
+    // helper function for sending an AJAX request
+    function ajax(method, url, data, success, error) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                success(xhr.response, xhr.responseType);
+            } else {
+                error(xhr.status, xhr.response, xhr.responseType);
+            }
+        };
+        xhr.send(data);
+    }
 }
